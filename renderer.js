b2c10a8e8e8a414246b32080a4f1e17ab35f35e1
raksa <eng.raksa@gmail.com>
2017-03-23 17:06:32 +0700

    update

--- renderer.js
+++ renderer.js
@@ -2,15 +2,15 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('app.sqlite3');
  
 db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
+  db.run("CREATE TABLE info_table (info TEXT)");
  
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  var stmt = db.prepare("INSERT INTO info_table VALUES (?)");
   for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
+      stmt.run("Info " + i);
   }
   stmt.finalize();
  
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+  db.each("SELECT rowid AS id, info FROM info_table", function(err, row) {
       console.log(row.id + ": " + row.info);
   });
 });
