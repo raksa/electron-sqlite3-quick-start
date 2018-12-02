661006b753687359c4afc0d0cb1d2bc9f12f77a5
raksa <eng.raksa@gmail.com>
2018-12-02 22:09:45 +0700

    update to ES6

--- renderer.js
+++ renderer.js
@@ -1,18 +1,20 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('app.sqlite3');
- 
-db.serialize(function() {
-  db.run("CREATE TABLE info_table (info TEXT)");
- 
-  var stmt = db.prepare("INSERT INTO info_table VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Info " + i);
-  }
-  stmt.finalize();
- 
-  db.each("SELECT rowid AS id, info FROM info_table", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('app.sqlite3');
+
+db.serialize(() => {
+    db.run("CREATE TABLE info_table (info TEXT)", e => {
+        // console.error(e);
+    });
+
+    const stmt = db.prepare("INSERT INTO info_table VALUES (?)");
+    for (let i = 0; i < 10; i++) {
+        stmt.run("Info " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM info_table", (err, row) => {
+        console.log(row.id + ": " + row.info);
+    });
 });
- 
+
 //db.close();
