e52c2bf440a6431c63b78c56e36792a2bfd1b440
Adam Solchenberger <asolchenberger@testtech-solutions.com>
2019-08-15 23:06:25 -0500

    added nodeIntegration back into electron

--- main.js
+++ main.js
@@ -1,43 +1,31 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800, height: 600,
+      webPreferences: {
+          nodeIntegration: true
+      }
+  });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'))
+   // Open the DevTools.
+   mainWindow.webContents.openDevTools()
+     // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
   })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
